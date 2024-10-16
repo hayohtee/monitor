ca2a45b028b216a1f65c234dae8a61d292a55200
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-16 22:36:04 +0100

    Update main.go to display hardware infos on the terminal

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main()  {
-	
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/hayohtee/monitor/internal/hardware"
+)
+
+func main() {
+	log.Println("starting system monitor")
+	go func() {
+		for {
+			systemSection, err := hardware.GetSystemSection()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			cpuSection, err := hardware.GetCPUSection()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			diskSection, err := hardware.GetDiskSection()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println(systemSection)
+			fmt.Println(diskSection)
+			fmt.Println(cpuSection)
+
+			time.Sleep(3 * time.Second)
+		}
+	}()
+	time.Sleep(5 * time.Minute)
 }
